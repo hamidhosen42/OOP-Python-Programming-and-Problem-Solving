bcae4be666ca5cdbfe130d603abfb62a71184046
Md.Hamid Hosen <mdhamidhosen8444@gmail.com>
2023-12-18 09:40:49 +0600

    Create many Fixed route out of flexible routes

--- 06-Week 06-Airport Flight Scheduler/Module 21-Purchase Ticket & Schedule Tip on Flight Schedule/flight_scheduler/All_Airports.py
+++ 06-Week 06-Airport Flight Scheduler/Module 21-Purchase Ticket & Schedule Tip on Flight Schedule/flight_scheduler/All_Airports.py
@@ -12,35 +12,40 @@ class AllAirports:
         currency_rates = {}
         country_currency = {}
 
-        #! get currency name <---> rate
-        with open('./data/currencyrates.csv', 'r') as file:
+        # get currency name <---> rate
+        with open('./data/airport.csv', 'r') as file:
             lines = csv.reader(file)
             for line in lines:
+                # print(line)
                 currency_rates[line[1]] = line[2]
         file.close()
 
-        #! dictionary country <---> currency name
+        # dictionary country <---> currency name
         with open('./data/countrycurrency.csv', 'r') as file:
             lines = csv.reader(file)
             for line in lines:
                 country_currency[line[0]] = line[1]
         file.close()
 
-        #! create airport
+        # create airport
         with open(file_path, 'r', encoding="utf8") as file:
             lines = csv.reader(file)
             try: 
                 for line in lines:
                     country = line[3]
+                    if country not in country_currency:
+                        continue
                     currency = country_currency[country]
+                    if currency not in currency_rates:
+                        continue
                     rate = currency_rates[currency]
                     airports[line[4]] = Airport(line[4], line[1], line[2], line[3], line[6], line[7], rate)
             except KeyError as e:
                 print('key not found', e)
 
             self.airports  = airports
-            #! for airport in airports.items():
-            #!     print(airport)
+            # for airport in airports.items():
+            #     print(airport)
     
     def get_distance_between_two_airports(self, lat1, lon1, lat2, lon2):
         radius = 6371
